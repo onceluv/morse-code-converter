d31248d8458d8f0a47aa9dc3472d557dd468063f
Vitor <vitorscorner@hotmail.com>
2024-12-08 16:54:17 -0300

    Implement csv module to main program

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-# Set dictionary to insert the morse code key and values
-morse_dict = {" ": "/"} # The '/' will replace the space
+import csv
 
 # Open the .csv file containing the morse code
 with open("morse.csv", "r") as file:
-    # Iterate through each row and add them as key and values in the dictionary
-    for row in file:
-        key, value = row.split(",") # Split the row and save the key and value separately
-        morse_dict[key] = value[:-1] # Remove the '\n' from the values
+    morse_raw = csv.reader(file) # Use the csv module in Python the read the .csv file
+    morse_dict = {row[0]: row[1] for row in morse_raw} # Convert the .csv file to dictionary
 
 # Ask for user input
-original_string = input("Write what you want to convert to Morse Code: ")
+original_string = input("Text to convert to Morse Code: ")
 
 # Create a list with the encoded version of the user input
 morse_string = [morse_dict[char] if char in morse_dict else char for char in original_string.upper()]
